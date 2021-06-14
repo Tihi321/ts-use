bfa9eefa1c2d937f0757d6eb64c760bcc90a1c02
Tihomir Selak <tknox.dr@gmail.com>
2021-06-14 17:01:45 +0200

    TS-11: Build lib, bump version

--- esm/useState/useState.js
+++ esm/useState/useState.js
@@ -1,4 +1,5 @@
-import { useEffect, useMemo, useState } from "react";
+import { useState } from "react";
+import { isEqual } from "lodash";
 /**
  * Hook for reactive state, state that react to the changes from initialState
  * @example
@@ -7,11 +8,12 @@ import { useEffect, useMemo, useState } from "react";
  * @return {array} returns state and setState from react
  */
 export var useReactiveState = function (propState) {
-    var _a = useState(propState), state = _a[0], setState = _a[1];
-    var propMemoState = useMemo(function () { return propState; }, [propState]);
-    useEffect(function () {
-        setState(propMemoState);
-    }, [propMemoState]);
+    var _a = useState(propState), passedValue = _a[0], setPassedValue = _a[1];
+    var _b = useState(propState), state = _b[0], setState = _b[1];
+    if (!isEqual(passedValue, propState)) {
+        setPassedValue(propState);
+        setState(propState);
+    }
     return [state, setState];
 };
 //# sourceMappingURL=useState.js.map
