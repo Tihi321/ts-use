f3a7b7b7ae2e32ccf44fe20965361279a0bc592a
Tihomir Selak <tknox.dr@gmail.com>
2021-09-19 17:46:12 +0200

    TS-15: Add query builder to fetch

---
+++ src/typings/fetch/fetch.ts
@@ -0,0 +1,14 @@
+import { RequestInit } from "node-fetch";
+
+export type TFetchQuery = {
+  key: string;
+  value?: string;
+};
+
+export type TFetchUrl =
+  | string
+  | {
+      url: string;
+      options?: RequestInit;
+      query?: TFetchQuery[];
+    };
--- src/useMount/useMount.ts
+++ src/useMount/useMount.ts
@@ -1,4 +1,4 @@
-import { isFunction } from "lodash";
+import isFunction from "lodash/isFunction";
 import { useEffect, useState } from "react";
 
 type TUseMountOptions = {
